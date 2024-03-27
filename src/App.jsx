5e3195a872b73e92d86523fce316caa75bcbb178
Acron1k <danilm.5@yandex.ru>
2024-03-27 17:19:12 +0500

    todo v 1.2. Добавил красивое закрытие задач, тени.

--- src/App.jsx
+++ src/App.jsx
@@ -36,21 +36,44 @@ function App() {
 			</form>
 			<br />
 			<div className={styles.todosList}>
-				{isLoading || isCreating || isDeleting || isUpdating ? (
+				{isLoading || isCreating || isDeleting ? (
 					<div className={styles.loader}></div>
 				) : (
 					Object.entries(todos).map(([id, { title, completed }]) => (
 						<div key={id} className={styles.todoElement}>
-							<p className={styles.todoText}>{title}</p>
-							<input
-								type="checkbox"
-								checked={completed}
-								onChange={() => {
-									toggleTodoCompletion(id, !completed);
-								}}
-								disabled={isUpdating}
-								className={styles.todoControl}
-							/>
+							<div className={styles.checkboxContainer}>
+								<input
+									type="checkbox"
+									className={styles.check}
+									onChange={() => toggleTodoCompletion(id, !completed)}
+									checked={completed}
+									id={id}
+									disabled={isUpdating}
+								/>
+								<label htmlFor={id} className={styles.label}>
+									<svg width="500" height="50" viewBox="0 0 500 50" className={styles.svg}>
+										<rect
+											x="15"
+											y="20"
+											width="25"
+											height="25"
+											stroke="black"
+											fill="none"
+											strokeOpacity=".5"
+										/>
+										<g transform="translate(0,-1002.3622)">
+											<path
+												d="m 15,1033 c 11,-9 27,2 39,-1 4,-1 7,-3 12,-3 11,0 20,2 30,7 5,2 42,2 48,0 4,-2 8,-3 13,-6 7,-3 26,0 35,0 14,0 28,-3 42,-3 37,0 73,6 107,-2 6,-1 18,-0.01 24,2 3,1 7,-0 11,0 21,4 42,2 63,-4 14,-4 49,12 55,7"
+												stroke="black"
+												fill="none"
+												strokeWidth="3"
+												className={styles.path2}
+											/>
+										</g>
+									</svg>
+								</label>
+							</div>
+							<span className={styles.taskText}>{title}</span>
 							<button
 								disabled={isDeleting}
 								onClick={() => requestDeleteToDo(id)}
@@ -60,6 +83,26 @@ function App() {
 							</button>
 						</div>
 					))
+					// Object.entries(todos).map(([id, { title, completed }]) => (
+					// 	<div key={id} className={styles.todoElement}>
+					// 		<p className={styles.todoText}>{title}</p>
+					// 		<input
+					// 			type="checkbox"
+					// 			checked={completed}
+					// 			onChange={() => {
+					// 				toggleTodoCompletion(id, !completed);
+					// 			}}
+					// 			disabled={isUpdating}
+					// 			className={styles.todoControl}
+					// 		/>
+					// 		<button
+					// 			disabled={isDeleting}
+					// 			onClick={() => requestDeleteToDo(id)}
+					// 			className={styles.todoControl}
+					// 		>
+					// 			Удалить
+					// 		</button>
+					// 	</div>
 				)}
 			</div>
 		</div>
